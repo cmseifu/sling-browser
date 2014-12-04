24a8f5c09846ade9b7d742138035a164c615891f
Sheng Huang <cmseifu@gmail.com>
2014-12-04 17:02:54 -0500

    common code for storage

---
+++ src/main/resources/SLING-CONTENT/apps/browser/static/common.js
@@ -0,0 +1,66 @@
+ /* Local Storage methods, persist to host */
+   /* Use check if localStorage is supported */
+	function hasLocalStorage() {
+		return (typeof localStorage !== "undefined" && localStorage != null);
+	}
+   
+	/* Retrieve the string from storage */
+   function getLocalStorage(key) {
+	   return hasLocalStorage() ? localStorage.getItem(key) : null;
+   }
+   
+   /* Retrive the JSON object from storage */
+	function getJsonLocalStorage(key) {
+		try {
+			return JSON.parse(getLocalStorage(key));
+		} catch (e) {
+			console.log(key+" does not seem to have JSON format, try use getFromLocalStorage instead ("+e+")");
+		}
+	}
+	
+	/* Set to storage, if object it will be stringified */
+	function setLocalStorage(key, value) {
+		if (hasLocalStorage()) {
+			localStorage.setItem(key, typeof value == 'string' ? value : JSON.stringify(value));
+		}
+	}
+	/* Remove the key from storage */
+	function clearLocalStorage(key) {
+		if (hasLocalStorage()) {
+			localStorage.removeItem(key);
+		}
+	}
+	
+	/* Session Storage methods, persist only through out the life of the browse tab */
+   /* Use check if sessionStorage is supported */
+	function hasSessionStorage() {
+		return (typeof sessionStorage !== "undefined" && sessionStorage != null);
+	}
+   
+	/* Retrieve the string from storage */
+   function getSessionStorage(key) {
+	   return hasSessionStorage() ? sessionStorage.getItem(key) : null;
+   }
+   
+   /* Retrive the JSON object from storage */
+	function getJsonSessionStorage(key) {
+		try {
+			return JSON.parse(getSessionStorage(key));
+		} catch (e) {
+			console.log(key+" does not seem to have JSON format, try use getFromSessionStorage instead ("+e+")");
+		}
+		
+	}
+	
+	/* Set to storage, if object it will be stringified */
+	function setSessionStorage(key, value) {
+		if (hasSessionStorage()) {
+			sessionStorage.setItem(key, typeof value === 'string' ? value : JSON.stringify(value));
+		}
+	}
+	/* Remove the key from storage */
+	function clearSessionStorage(key) {
+		if (hasSessionStorage()) {
+			sessionStorage.removeItem(key);
+		}
+	}
