a09f6855fdc89ffd90961eb496cbf55f5fec552a
Sheng Huang <cmseifu@gmail.com>
2014-12-08 15:29:02 -0500

    moved code to common.js

--- src/main/resources/SLING-CONTENT/apps/browser/static/edit/properties.js
+++ src/main/resources/SLING-CONTENT/apps/browser/static/edit/properties.js
@@ -1,16 +1,4 @@
-/* Extend jQuery Shake */
-jQuery.fn.shake = function(intShakes, intDistance, intDuration) {
-    this.each(function() {
-        $(this).css("position","relative"); 
-        for (var x=1; x<=intShakes; x++) {
-        $(this).animate({left:(intDistance*-1)}, (((intDuration/intShakes)/3)))
-    .animate({left:intDistance}, ((intDuration/intShakes)/2))
-    .animate({left:0}, (((intDuration/intShakes)/3)));
-    }
-  });
-    return this;
-};
-/* End extend */
+
 
 var SESSION_KEY = 'browser-property';
 function toggleLock() {
