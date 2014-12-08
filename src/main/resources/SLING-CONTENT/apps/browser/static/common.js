cd03440680f46bb1cdb9b3401e6af7ac94a90b86
Sheng Huang <cmseifu@gmail.com>
2014-12-08 15:28:50 -0500

    jquery context menu extend

--- src/main/resources/SLING-CONTENT/apps/browser/static/common.js
+++ src/main/resources/SLING-CONTENT/apps/browser/static/common.js
@@ -1,4 +1,78 @@
- /* Local Storage methods, persist to host */
+// jQuery extend
+(function ($, window) {
+	// Context menu from http://jsfiddle.net/KyleMit/X9tgY/
+    $.fn.contextMenu = function (settings) {
+        return this.each(function () {
+            // Open context menu
+            $(this).on("contextmenu", function (e) {
+                //open menu
+                $(settings.menuSelector)
+                    .data("invokedOn", $(e.target))
+                    .show()
+                    .css({
+                        position: "absolute",
+                        left: getLeftLocation(e),
+                        top: getTopLocation(e)
+                    })
+                    .off('click')
+                    .on('click', function (e) {
+                        $(this).hide();
+                        var $invokedOn = $(this).data("invokedOn");
+                        var $selectedMenu = $(e.target);
+                        settings.menuSelected.call(this, $invokedOn, $selectedMenu);
+                });
+                return false;
+            });
+
+            //make sure menu closes on any click
+            $(document).click(function () {
+                $(settings.menuSelector).hide();
+            });
+        });
+
+        function getLeftLocation(e) {
+            var mouseWidth = e.pageX;
+            var pageWidth = $(window).width();
+            var menuWidth = $(settings.menuSelector).width();
+            // opening menu would pass the side of the page
+            if (mouseWidth + menuWidth > pageWidth &&
+                menuWidth < mouseWidth) {
+                return mouseWidth - menuWidth;
+            } 
+            return mouseWidth;
+        }        
+        
+        function getTopLocation(e) {
+            var mouseHeight = e.pageY;
+            var pageHeight = $(window).height();
+            var menuHeight = $(settings.menuSelector).height();
+            // opening menu would pass the bottom of the page
+            if (mouseHeight + menuHeight > pageHeight &&
+                menuHeight < mouseHeight) {
+                return mouseHeight - menuHeight;
+            } 
+            return mouseHeight;
+        }
+
+    };
+    
+    /* Shake from http://jsfiddle.net/Webveloper/HjFUK/ */
+    $.fn.shake = function(intShakes, intDistance, intDuration) {
+        this.each(function() {
+            $(this).css("position","relative"); 
+            for (var x=1; x<=intShakes; x++) {
+            $(this).animate({left:(intDistance*-1)}, (((intDuration/intShakes)/3)))
+        .animate({left:intDistance}, ((intDuration/intShakes)/2))
+        .animate({left:0}, (((intDuration/intShakes)/3)));
+        }
+      });
+        return this;
+    };
+})(jQuery, window);
+/* End extend */
+
+
+/* Local Storage methods, persist to host */
    /* Use check if localStorage is supported */
 	function hasLocalStorage() {
 		return (typeof localStorage !== "undefined" && localStorage != null);
